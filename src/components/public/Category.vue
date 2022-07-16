178ce4519b4f319936aa88e9bffe79fd412115c7
Myo Min Htoo <myominhtoo2003@gmail.com>
2022-07-16 19:07:26 +0630

    changed category box as capsule style

--- src/components/public/Category.vue
+++ src/components/public/Category.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link :to="{ path : '/news' , query : { 'category' : id } }"  class="p-2 shadow-sm text-danger text-decoration-none text-capitalize fw-bold ">{{ text }}</router-link>
+    <router-link :to="{ path : '/news' , query : { 'category' : id } }"  class="p-2 shadow-sm text-danger text-decoration-none text-capitalize fw-bold" id="category">{{ text }}</router-link>
 </template>
 
 <script>
